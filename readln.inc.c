d63a104707f15b129db788044b36b5fe3974400e
gereoffy <arpi.git@interoot.hu>
2003-02-20 00:05:43 +0000

    revision 1.4 date: 2003/02/20 00:05:43;  author: arpi;  state: Exp;  lines: +11 -8 ->read_line2, it handles multi-line headers ----------------------------

--- readln.inc.c
+++ readln.inc.c
@@ -20,14 +20,15 @@ int puffer_update(){
   eof_jel=1;return 0;
 }
 
-int readln_sor() {
+int readln_sor2(int mfs, int header) {
 int i=0;
 register char c;
   sor_pos=puffer_pos+puffer_mut;
   sor_next=0;
+again:
   if(sor_pos>=eol_pos){eol_jel=1;sor[0]=0;return(-1);}
   
-  if(folder->mfs!=MFS_PMM && puffer_mut+sormaxsize+2<puffer_size){
+  if(mfs!=MFS_PMM && puffer_mut+(sormaxsize-i)+2<puffer_size){
     /* Optimized version! */
     register char *p=&puffer[puffer_mut];
     do{
@@ -52,20 +53,18 @@ register char c;
       c=p[15]; if(c==10) break; sor[i++]=c;
       p+=16;
 #endif      
-    }while(i<sormaxsize);
+    }while(i<sormaxsize-18);
     puffer_mut+=i+1;
     if(i && sor[i-1]==13) --i;
     sor[i]=0;
-    sor_next=puffer[puffer_mut];
-    return 0;
-  }
+  } else {
   
   do{
     if(puffer_size<=puffer_mut){
         if(!puffer_update()){sor[i]=0;return(-2);}
     }
     c=puffer[puffer_mut++];
-    if(c==26){eol_jel=1;return(-1);}
+    if(c==26 && mfs==MFS_PMM){eol_jel=1;return(-1);}
     if(c==10) break;
     if(c!=13) sor[i++]=c;
   }while(i<sormaxsize);
@@ -73,10 +72,14 @@ register char c;
   if(puffer_size<=puffer_mut)
     if(!puffer_update()){ // no more data...
       eof_jel=0; // clear eof flag, as we're not at EOF yet
-      sor_next=0;
       return 0;
     }
+
+  }
   sor_next=puffer[puffer_mut];
+  if(header && (sor_next==9 || sor_next==32)){ // multi-line header
+    goto again;
+  }
   return(0);
 }
 
