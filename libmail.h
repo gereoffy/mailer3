32c609313b2dbaa5ba08d896c0c0b4c4bb7117ba
gereoffy <arpi.git@interoot.hu>
2003-02-20 16:20:59 +0000

    revision 1.5 date: 2003/02/20 16:20:59;  author: arpi;  state: Exp;  lines: +4 -20 cleanup - made most private stuff 'static' - remoev some globals to local - merge/remove some redundant functions (free_folder, load_folder) - move readln_sor2() initialization to folder_seek() ----------------------------

--- libmail.h
+++ libmail.h
@@ -40,9 +40,6 @@ typedef struct {
   FILE *file_strings;
 } folder_st;
 
-extern rek_st mail;
-extern folder_st *folder;
-
 /*****************************************************************************/
 #define MAX_MIMEPARTS 32
 #define MIME_MAXLEN 40
@@ -75,17 +72,16 @@ extern char message_id[];
 extern char sor[sormaxsize];
 extern char sor2[sormaxsize];
 
-#define folder_seek(folder,i) fseek(folder->file_folder,i,SEEK_SET)
+extern int open_folder(folder_st* folder,char *folder_name,char *index_name,char *strings_name);
+extern void close_folder(folder_st *folder);
+
 extern int read_rek(folder_st *folder,int i,rek_st *mail);
 extern int write_rek(folder_st *folder,int i,rek_st *mail);
-extern int open_folder(folder_st* folder,char *folder_name,char *index_name,char *strings_name);
-extern int load_folder(folder_st *folder);
-extern void free_folder(folder_st *folder);
-extern void close_folder(folder_st *folder);
 
 extern void open_mail(folder_st *folder,rek_st *mail);
 extern void save_part(folder_st *folder,int i,FILE *f2,char *replystr,int skip_header,int linewrap);
 extern int save_attachment(folder_st *folder,int i,FILE *ff);
+extern void save_mail_source(folder_st *folder,rek_st *mail, char *fnev);
 
 extern char* cim_ertelmezo(char *sor,int from_mod);
 extern void kodlap_generalo();
@@ -100,16 +96,4 @@ extern char* copy(char* hova,char* mibol,int p,int l);
 extern char* upcstr(char* s2,char* s1);
 extern int strpos(char* s1,char* s2);
 extern void get_date(char* _date);
-
-
 /*****************************************************************************/
-
-
-
-
-
-
-
-
-
-
